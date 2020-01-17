901afedfa729916ae9b3fcee70f356f7bf6cf923
uihyeon.cha <chad@i-um.net>
2020-01-17 18:55:29 +0900

    lifecycle 적용

--- components/LadderGame.tsx
+++ components/LadderGame.tsx
@@ -1,35 +1,50 @@
-import React, { useRef, useEffect, useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Typography from '@material-ui/core/Typography'
 import Box from '@material-ui/core/Box'
 import { InitialState } from '~/reducers/index.type'
+import { useLifecycle } from '~/hooks/lifecycle'
+
+export const defaultOption = {
+    mapMinHeight: 300,
+}
+
+export const initialState = {
+    isPaintingLadder: false,
+    isPaintedLadder: false,
+    mapWidth: 0,
+    mapHeight: defaultOption.mapMinHeight,
+}
 
 const LadderGame: React.FC<InitialState> = props => {
-    const defaultOption = {
-        minHeight: 300,
-    }
-    const initialState = {
-        mounted: false,
-        width: 0,
-        height: defaultOption.minHeight,
-        isPaintingLadder: false,
-        isPaintedLadder: false,
-    }
-
-    const wrapperRef = useRef<HTMLDivElement>(null)
+    const { useCreated, useMounted, useUpdated } = useLifecycle()
     const [state, setState] = useState(initialState)
 
+    const wrapperRef = useRef<HTMLDivElement>(null)
+
     const calcMapSize = () => {
-        console.log(wrapperRef.current?.clientWidth)
+        setState({
+            ...state,
+            mapWidth: wrapperRef.current?.clientWidth || 0,
+            mapHeight: Math.max(state.mapHeight, wrapperRef.current?.clientHeight || 0),
+        })
     }
 
-    useEffect(() => {
-        if (state.mounted) {
-            calcMapSize()
-        }
-        console.log(state)
+    useCreated(() => {
+        console.group('Created LadderGame')
+        calcMapSize()
+        console.groupEnd()
     })
 
-    if (!state.mounted) setState(state => ({ ...state, mounted: true }))
+    useMounted(() => {
+        console.group('Mounted LadderGame')
+        console.log(state)
+        console.groupEnd()
+    })
+
+    useUpdated(() => {
+        console.group('Updated LadderGame')
+        console.groupEnd()
+    })
 
     return (
         <div ref={wrapperRef}>
