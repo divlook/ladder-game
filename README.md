1d63fdb854afa0448fa1b6bc27a5cd3b6c78ecaf
uihyeon.cha <uihyeon.cha@gmail.com>
2020-01-25 01:16:19 +0900

    배포 설정

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # README
+
+배포
+
+```bash
+pm2 deploy pm2.config.js production [setup]
+```
---
+++ pm2.config.js
@@ -0,0 +1,35 @@
+/* eslint-disable */
+
+module.exports = {
+    apps: [
+        {
+            name: 'ladder-game (FE)',
+            script: 'npm run start',
+
+            // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '1G',
+            env: {
+                NODE_ENV: 'development',
+            },
+            env_production: {
+                NODE_ENV: 'production',
+            },
+        },
+    ],
+
+    deploy: {
+        production: {
+            key  : '~/Documents/pem/chad.pem',
+            user: 'ubuntu',
+            host: '132.145.88.159',
+            ref: 'origin/master',
+            repo: 'git@github.com:divlook/ladder-game.git',
+            path: '/home/ubuntu/app/ladder-game/production',
+            'pre-setup': 'mkdir -p /home/ubuntu/app/ladder-game/production',
+            'post-deploy': 'npm install --only=prod --no-audit && pm2 reload pm2.config.js --env production',
+        },
+    },
+}
