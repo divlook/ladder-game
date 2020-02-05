39881117dd93b7133229358a870ddc56958ba3fd
uihyeon.cha <chad@i-um.net>
2020-02-05 18:57:06 +0900

    애니메이션 작업 중

--- components/ResultCanvas.tsx
+++ components/ResultCanvas.tsx
@@ -85,29 +85,56 @@ const ResultCanvas: React.FC<ResultCanvasProps> = props => {
         }
 
         for (let index = 0, len = coordinates.length; index < len; index++) {
-            const xy = coordinates[index]
+            const currentCoordinate = coordinates[index]
+            const prevCoordinate = coordinates[index - 1]
+
+            // TODO: 이 사이트 참고해서 애니메이션 작업
+            // https://stackoverflow.com/questions/23939588/how-to-animate-drawing-lines-on-canvas
 
             if (index === 0) {
                 ctx.beginPath()
-                ctx.arc(xy[0], xy[1] - 8, 6, 0, Math.PI * 2)
+                ctx.arc(currentCoordinate[0], currentCoordinate[1] - 8, 6, 0, Math.PI * 2)
                 ctx.closePath()
                 ctx.fill()
                 ctx.beginPath()
-                ctx.moveTo(xy[0], xy[1] - 3)
-                ctx.lineTo(...xy)
+                ctx.moveTo(currentCoordinate[0], currentCoordinate[1] - 3)
+                // ctx.lineTo(...xy)
+                calcWaypoints([currentCoordinate[0], currentCoordinate[1] - 3], currentCoordinate, (waypoint: [number, number]) =>
+                    ctx.lineTo(...waypoint)
+                )
             } else {
-                ctx.lineTo(...xy)
+                // ctx.lineTo(...currentCoordinate)
+                calcWaypoints(prevCoordinate, currentCoordinate, (waypoint: [number, number]) =>
+                    ctx.lineTo(...waypoint)
+                )
             }
 
             if (index === len - 1) {
-                ctx.lineTo(xy[0], xy[1] + 3)
+                // ctx.lineTo(currentCoordinate[0], currentCoordinate[1] + 3)
+                calcWaypoints(prevCoordinate, [currentCoordinate[0], currentCoordinate[1] + 3], (waypoint: [number, number]) =>
+                    ctx.lineTo(...waypoint)
+                )
                 ctx.stroke()
                 ctx.beginPath()
-                ctx.arc(xy[0], xy[1] + 8, 6, 0, Math.PI * 2)
+                ctx.arc(currentCoordinate[0], currentCoordinate[1] + 8, 6, 0, Math.PI * 2)
                 ctx.closePath()
                 ctx.fill()
             }
         }
+    }
+
+    function calcWaypoints(start: number[], end: number[], cb?) {
+        const waypoints: [number, number][] = []
+        const dx = end[0] - start[0]
+        const dy = end[1] - start[1]
+        for (let i = 0; i < 100; i++) {
+            const x = start[0] + (dx * i) / 100
+            const y = start[1] + (dy * i) / 100
+            const waypoint: [number, number] = [x, y]
+            waypoints.push(waypoint)
+            cb?.(waypoint)
+        }
+        return waypoints
     }
 
     return <canvas ref={canvasRef} width={width} height={height} />
