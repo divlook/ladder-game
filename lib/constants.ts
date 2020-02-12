fb88f6faf572d191749244ba753bca17e2837d5b
uihyeon.cha <uihyeon.cha@gmail.com>
2020-02-13 01:00:07 +0900

    sentry는 production에서만 사용

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,11 @@
 import { version } from '~/package.json'
 
 export const NODE_ENV = process.env.NODE_ENV ?? 'development'
+export const IS_PRODUCTION = NODE_ENV === 'production'
 export const LADDER_VERSION = version ?? '0.1.0'
+
+/*
+ * plugins
+ */
 export const SENTRY_DSN = process.env.LADDER_PLUGIN_SENTRY_DSN ?? ''
+export const USE_SENTRY = SENTRY_DSN && IS_PRODUCTION
