353b0a21fef9f674ea2462a6de3b7af4c1ef5376
uihyeon.cha <chad@i-um.net>
2020-02-12 18:24:10 +0900

    소스맵 생성은 build 할때만 사용

--- next.config.js
+++ next.config.js
@@ -1,87 +1,91 @@
 /* eslint-disable */
 
 const path = require('path')
+const { PHASE_PRODUCTION_BUILD } = require('next/constants')
 const isProd = process.env.NODE_ENV === 'production'
-const withSourceMaps = require('@zeit/next-source-maps')
 require('./lib/loadenv')('plugin')
 
-const configWrappers = [withSourceMaps]
+module.exports = phase => {
+    const configWrappers = []
 
-const nextConfig = {
-    /**
-     * Environment Variables
-     */
-    env: {
-        LADDER_PLUGIN_SENTRY_DSN: process.env.LADDER_PLUGIN_SENTRY_DSN,
-    },
+    if (phase === PHASE_PRODUCTION_BUILD) {
+        configWrappers.push(require('@zeit/next-source-maps'))
+    }
 
-    /**
-     * Build directory
-     */
-    distDir: isProd ? 'build' : '.next',
-
-    /**
-     * Static HTML
-     *
-     * @see https://nextjs.org/docs/api-reference/next.config.js/exportPathMap
-     */
-    exportPathMap: async function(defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
-        return {
-            // "생성될 pathname": { page: "pages의 경로", query?: InitialProps }
-            '/': { page: '/' },
-        }
-    },
-
-    /**
-     * URL 뒤에 Slash 존재 여부
-     *
-     * @see https://nextjs.org/docs/api-reference/next.config.js/exportPathMap#adding-a-trailing-slash
-     * @example
-     * exportTrailingSlash ? "/about/index.html" : "/about.html"
-     */
-    exportTrailingSlash: true,
-
-    /**
-     * @see https://nextjs.org/docs/api-reference/next.config.js/custom-webpack-config
-     */
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-        if (!config.resolve) config.resolve = {}
-        if (!config.resolve.alias) config.resolve.alias = {}
-        if (!config.module) config.module = {}
-        if (!config.module.rules) config.module.rules = []
-
-        /*
-         * plugins
+    return configWrappers.reduce((config, wrapper) => wrapper(config), {
+        /**
+         * Environment Variables
          */
-        config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
+        env: {
+            LADDER_PLUGIN_SENTRY_DSN: process.env.LADDER_PLUGIN_SENTRY_DSN,
+        },
 
-        /*
-         * alias
+        /**
+         * Build directory
          */
-        config.resolve.alias = {
-            ...config.resolve.alias,
-            '~': path.resolve(__dirname, './'),
-        }
+        distDir: isProd ? 'build' : '.next',
 
-        /*
-         * rules
+        /**
+         * Static HTML
+         *
+         * @see https://nextjs.org/docs/api-reference/next.config.js/exportPathMap
          */
-        if (isServer) {
-            config.module.rules.push({
-                enforce: 'pre',
-                test: /\.tsx?$/,
-                exclude: [/node_modules/, /\.next/],
-                loader: 'eslint-loader',
-                options: {
-                    failOnError: true,
-                    failOnWarning: false,
-                    cache: false,
-                },
-            })
-        }
+        exportPathMap: async function(defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
+            return {
+                // "생성될 pathname": { page: "pages의 경로", query?: InitialProps }
+                '/': { page: '/' },
+            }
+        },
 
-        return config
-    },
+        /**
+         * URL 뒤에 Slash 존재 여부
+         *
+         * @see https://nextjs.org/docs/api-reference/next.config.js/exportPathMap#adding-a-trailing-slash
+         * @example
+         * exportTrailingSlash ? "/about/index.html" : "/about.html"
+         */
+        exportTrailingSlash: true,
+
+        /**
+         * @see https://nextjs.org/docs/api-reference/next.config.js/custom-webpack-config
+         */
+        webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+            if (!config.resolve) config.resolve = {}
+            if (!config.resolve.alias) config.resolve.alias = {}
+            if (!config.module) config.module = {}
+            if (!config.module.rules) config.module.rules = []
+
+            /*
+             * plugins
+             */
+            config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
+
+            /*
+             * alias
+             */
+            config.resolve.alias = {
+                ...config.resolve.alias,
+                '~': path.resolve(__dirname, './'),
+            }
+
+            /*
+             * rules
+             */
+            if (isServer) {
+                config.module.rules.push({
+                    enforce: 'pre',
+                    test: /\.tsx?$/,
+                    exclude: [/node_modules/, /\.next/],
+                    loader: 'eslint-loader',
+                    options: {
+                        failOnError: true,
+                        failOnWarning: false,
+                        cache: false,
+                    },
+                })
+            }
+
+            return config
+        },
+    })
 }
-
-module.exports = configWrappers.reduce((config, wrapper) => wrapper(config), nextConfig)
