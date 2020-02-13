338c7e41523284e2cb4f8376f60ef2db2bb89281
uihyeon.cha <chad@i-um.net>
2020-02-13 12:23:18 +0900

    변수명 수정

--- lib/constants.ts
+++ lib/constants.ts
@@ -8,10 +8,10 @@ export const LADDER_VERSION = version ?? '0.1.0'
  * plugins
  */
 export const SENTRY_DSN = process.env.LADDER_PLUGIN_SENTRY_DSN ?? ''
-export const USE_SENTRY = SENTRY_DSN && IS_PRODUCTION
+export const USING_SENTRY = SENTRY_DSN && IS_PRODUCTION
 
 export const LOGROCKET_ID = process.env.LADDER_PLUGIN_LOGROCKET_ID ?? ''
-export const USE_LOGROCKET = LOGROCKET_ID && IS_PRODUCTION
+export const USING_LOGROCKET = LOGROCKET_ID && IS_PRODUCTION
 
 export const GA_ID = process.env.LADDER_PLUGIN_GA_ID ?? ''
 export const USING_GA = GA_ID && IS_PRODUCTION
--- plugins/logrocket.ts
+++ plugins/logrocket.ts
@@ -1,9 +1,9 @@
 import * as LogRocket from 'logrocket'
 import setupLogRocketReact from 'logrocket-react'
-import { NODE_ENV, LADDER_VERSION, LOGROCKET_ID, USE_SENTRY, USE_LOGROCKET } from '~/lib/constants'
+import { NODE_ENV, LADDER_VERSION, LOGROCKET_ID, USING_SENTRY, USING_LOGROCKET } from '~/lib/constants'
 
 export const initLogRocket = () => {
-    if (USE_LOGROCKET) {
+    if (USING_LOGROCKET) {
         LogRocket.init(LOGROCKET_ID, {
             release: `${NODE_ENV}@${LADDER_VERSION}`,
         })
@@ -15,7 +15,7 @@ export const initLogRocket = () => {
 }
 
 export const withSentry = Sentry => {
-    if (USE_LOGROCKET && USE_SENTRY) {
+    if (USING_LOGROCKET && USING_SENTRY) {
         LogRocket.getSessionURL(sessionURL => {
             Sentry.configureScope(scope => {
                 scope.setExtra('sessionURL', sessionURL)
