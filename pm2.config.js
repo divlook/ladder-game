4ad9a2bc753121ecdf8c6916117351cb28eea3d6
uihyeon.cha <chad@i-um.net>
2020-02-18 10:34:37 +0900

    optional dependencies 설치 안함

--- pm2.config.js
+++ pm2.config.js
@@ -20,7 +20,7 @@ const deployConfig = {
     repo: LADDER_DEPLOY_REPO,
     path: path.join(LADDER_DEPLOY_PATH, NODE_ENV),
     'pre-setup': `mkdir -p ${path.join(LADDER_DEPLOY_PATH, NODE_ENV)}`,
-    'post-deploy': `npm install --only=prod && pm2 startOrRestart pm2.config.js --env ${NODE_ENV} && pm2 save`,
+    'post-deploy': `npm install --only=prod --no-optional && pm2 startOrRestart pm2.config.js --env ${NODE_ENV} && pm2 save`,
 }
 
 module.exports = {
