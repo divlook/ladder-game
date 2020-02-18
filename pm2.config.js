4acb9ff4b0d0fd7c3354573fb612fa77b7b099aa
uihyeon.cha <chad@i-um.net>
2020-02-18 18:29:46 +0900

    서버 위치 정할 수 있게 로직 수정

--- pm2.config.js
+++ pm2.config.js
@@ -1,27 +1,36 @@
 /* eslint-disable */
 
 const path = require('path')
-require('./lib/loadenv')('deploy')
+const { getServerNames } = require('./lib/utils-node')
+const serverNames = getServerNames()
+const deployConfigs = serverNames.reduce((result, serverName) => {
+    const env = require('./lib/loadenv')(`deploy/${serverName}`)
 
-const NODE_ENV = process.env.NODE_ENV || 'development'
-const LADDER_DEPLOY_SSH_KEY = process.env.LADDER_DEPLOY_SSH_KEY
-const LADDER_DEPLOY_USER = process.env.LADDER_DEPLOY_USER
-const LADDER_DEPLOY_HOST = process.env.LADDER_DEPLOY_HOST
-const LADDER_DEPLOY_REMOTE = process.env.LADDER_DEPLOY_REMOTE || 'origin'
-const LADDER_DEPLOY_BRANCH = process.env.LADDER_DEPLOY_BRANCH || 'master'
-const LADDER_DEPLOY_REPO = process.env.LADDER_DEPLOY_REPO
-const LADDER_DEPLOY_PATH = process.env.LADDER_DEPLOY_PATH
+    const NODE_ENV = process.env.NODE_ENV || 'production'
+    const LADDER_DEPLOY_SSH_KEY = env.LADDER_DEPLOY_SSH_KEY
+    const LADDER_DEPLOY_USER = env.LADDER_DEPLOY_USER
+    const LADDER_DEPLOY_HOST = env.LADDER_DEPLOY_HOST
+    const LADDER_DEPLOY_REMOTE = env.LADDER_DEPLOY_REMOTE || 'origin'
+    const LADDER_DEPLOY_BRANCH = env.LADDER_DEPLOY_BRANCH || 'master'
+    const LADDER_DEPLOY_REPO = env.LADDER_DEPLOY_REPO
+    const LADDER_DEPLOY_PATH = env.LADDER_DEPLOY_PATH
 
-const deployConfig = {
-    key: LADDER_DEPLOY_SSH_KEY,
-    user: LADDER_DEPLOY_USER,
-    host: LADDER_DEPLOY_HOST,
-    ref: `${LADDER_DEPLOY_REMOTE}/${LADDER_DEPLOY_BRANCH}`,
-    repo: LADDER_DEPLOY_REPO,
-    path: path.join(LADDER_DEPLOY_PATH, NODE_ENV),
-    'pre-setup': `mkdir -p ${path.join(LADDER_DEPLOY_PATH, NODE_ENV)}`,
-    'post-deploy': `npm install --only=prod --no-optional && pm2 startOrRestart pm2.config.js --env ${NODE_ENV} && pm2 save`,
-}
+    const deployConfig = {
+        key: LADDER_DEPLOY_SSH_KEY,
+        user: LADDER_DEPLOY_USER,
+        host: LADDER_DEPLOY_HOST,
+        ref: `${LADDER_DEPLOY_REMOTE}/${LADDER_DEPLOY_BRANCH}`,
+        repo: LADDER_DEPLOY_REPO,
+        path: path.join(LADDER_DEPLOY_PATH, NODE_ENV),
+        'pre-setup': `mkdir -p ${path.join(LADDER_DEPLOY_PATH, NODE_ENV)}`,
+        'post-deploy': `npm install --only=prod --no-optional && pm2 startOrRestart pm2.config.js --env ${NODE_ENV} && pm2 save`,
+    }
+
+    result[serverName] = deployConfig
+
+    return result
+}, {})
+
 
 module.exports = {
     apps: [
@@ -51,8 +60,6 @@ module.exports = {
     ],
 
     deploy: {
-        production: { ...deployConfig },
-        test: { ...deployConfig },
-        development: { ...deployConfig },
+        ...deployConfigs,
     },
 }
